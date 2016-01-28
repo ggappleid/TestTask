1d07b0df7305b66e4d95ecddba34ae6b0424e037
Kostiantyn Ishchenko <ggappleid@gmail.com>
2016-01-28 09:26:07 +0200

    Test task implementation

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+	public Transform Parent;
+
+	void Start()
+	{
+		Parent = this.transform.parent;
+	}
+}
---
+++ Assets/Scripts/SelectedItem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectedItem : MonoBehaviour
+{
+	private Item currentItem;
+
+	void OnDrop(GameObject go)
+	{
+		var item = go.GetComponent<Item>();
+
+		if (item == null || go.transform.parent.name == "Holder")
+		{
+			// We dragged non-item GameObject or non-dragged item
+			return;
+		}
+
+		// If we want to add or replace current Item
+		if (currentItem != item)
+		{
+			// Replacing current item with new
+			if (currentItem != null)
+			{
+				// Restore old hierarchy
+				currentItem.transform.SetParent(currentItem.Parent);
+
+				// Reset old position if possible
+				if (currentItem.Parent != null)
+				{
+					var grid = currentItem.Parent.GetComponent<UIGrid>();
+					grid.Reposition();
+				}
+			}
+
+			currentItem = item;
+		}
+	}
+}
